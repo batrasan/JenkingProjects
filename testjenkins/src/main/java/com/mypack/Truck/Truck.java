cd04ad436b71f1a01b6b3dbf5eacd2f50c7c8ca0
batrasan <cse07348.sbit@gmail.com>
2016-12-02 00:50:45 +0530

    one new Truck package is added

---
+++ testjenkins/src/main/java/com/mypack/Truck/Truck.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mypack.Truck;
+
+import com.mypack.Vehicle.Vehicle;
+
+/**
+ * @author Pillu
+ *
+ */
+public class Truck  extends Vehicle{
+
+	@Override
+	public void move() {
+		System.out.println("Truck is moving with high speed");
+		
+	}
+
+}
